b773964a867da94c9dd5b6ae516b56f4555d1cff
Rabuelvas <rabuelvas@uninorte.edu.co>
2022-04-06 14:22:12 -0500

    P2-S4:V3

--- src/public/scripts/scripts.js
+++ src/public/scripts/scripts.js
@@ -19,6 +19,14 @@ const lat = document.getElementById("lat");
 const time = document.getElementById("time");
 const date = document.getElementById("date");
 
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
 document.getElementById("start").addEventListener("change", function() {
     var inicio = this.value;
@@ -65,7 +73,7 @@ async function getData() {
     array.push(LatLng);
 
     if (marcador) marcador.setLatLng(LatLng)
-    else marcador = L.marker(LatLng).bindPopup('Usted está aquí').addTo(map)
+    else marcador = L.marker(LatLng,{icon: greenIcon}).bindPopup('Usted está aquí').addTo(map)
 
     if (polyline) {
         polyline.setLatLngs(array)
@@ -97,9 +105,9 @@ map.on('click', (e) =>{
     
     Vector_r = e.latlng
     console.log(Vector_r)
-
     if (circle) circle.setLatLng(Vector_r)
     else  circle=L.circle(Vector_r, {radius: 1100}).addTo(map);
+
 })
 
 const button2 = document.getElementById("button2");
